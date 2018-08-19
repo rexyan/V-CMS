f889ba3ad7a24c8306881a298a0033f9c6cab34c
rex_yan <rex_yan@126.com>
2018-08-19 15:55:35 +0800

    登录页简单完成

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -3,8 +3,26 @@
     <!-- 导航栏 -->
     <PageHeader :navList="getNavList"></PageHeader>
 
+    <!-- 登录主体部分 -->
+    <Row>
+      <Col span="6" class="mainBody">
+        <Form :label-width="80">
+          <FormItem label="用户名">
+            <Input type="text"></Input>
+          </FormItem>
+          <FormItem label="密码">
+            <Input type="password"></Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary">确定</Button>
+            <Button style="margin-left: 8px">取消</Button>
+          </FormItem>
+        </Form>
+      </Col>
+    </Row>
+
     <!-- 底部信息 -->
-    <PageFooter :footerInfo="getFooterInfo"></PageFooter>
+    <PageFooter class="footerFixed" :footerInfo="getFooterInfo"></PageFooter>
   </Layout>
 </template>
 <script>
@@ -13,9 +31,6 @@
   import PageFooter from "../../components/PageFooter";
 
   export default {
-    data() {
-      return {}
-    },
     components: {PageHeader, PageFooter},
     computed: {
       ...mapGetters([
@@ -23,6 +38,26 @@
         'getFooterInfo'
       ])
     },
+    data() {
+      return {}
+    },
     methods: {}
   }
 </script>
+
+
+<style scoped>
+  .mainBody {
+    margin: 5% auto auto auto;
+    position: absolute;
+    top: 0;
+    left: -40px;
+    right: 0;
+    bottom: 0;
+  }
+
+  .footerFixed {
+    position: fixed;
+    bottom: 0;
+  }
+</style>
