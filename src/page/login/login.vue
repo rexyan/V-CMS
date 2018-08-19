87ebebb39b265feb801032cd17f7adf29f8ec9ed
rex_yan <rex_yan@126.com>
2018-08-19 11:59:17 +0800

    登录页面

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,10 +1,22 @@
 <template>
-
+  <Layout>
+    <!-- 导航栏 -->
+    <PageHeader :navList="getNavList"></PageHeader>
+  </Layout>
 </template>
 <script>
+  import {mapGetters, mapActions, mapState, mapMutations} from 'Vuex';
+  import PageHeader from "../../components/PageHeader";
+
   export default {
     data() {
       return {}
+    },
+    components: {PageHeader},
+    computed: {
+      ...mapGetters([
+        'getNavList',
+      ])
     },
     methods: {}
   }
